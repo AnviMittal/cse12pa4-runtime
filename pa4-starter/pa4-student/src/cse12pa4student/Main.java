6111fe54eb7af6e44ae6e5b82e245f610a2cf2b0
AnviMittal <a2mittal@ucsd.edu>
2022-05-20 13:28:47 -0700

    dcd

--- pa4-starter/pa4-student/src/cse12pa4student/Main.java
+++ pa4-starter/pa4-student/src/cse12pa4student/Main.java
@@ -1,7 +1,11 @@
 package cse12pa4student;
+import java.util.List;
 public class Main {
 	
 	public static void main(String[] args) {
+
+		List<Measurement> test1 = Measure.measure(new String[]{"A", "B"}, 40, 100);
+		Measure.measurementsToCSV(test1); 
 		
 	}
 }
--- pa4-starter/pa4-student/src/cse12pa4student/Measure.java
+++ pa4-starter/pa4-student/src/cse12pa4student/Measure.java
@@ -1,5 +1,5 @@
 package cse12pa4student;
-
+import java.util.ArrayList;
 import java.util.List;
 import static cse12pa4mysteries.Mysteries.*;
 
@@ -7,19 +7,69 @@ public class Measure {
 
 
 	public static List<Measurement> measure(String[] toRun, int startN, int stopN) {
-		/** TODO **/
+
+		List<Measurement> list_of_measurements = new ArrayList<>(); 
+		for(int i = 0; i < toRun.length; i++){
+			String func = toRun[i]; 
+
+			for(int j = startN; j<= stopN; j++){
+
+
+				long startTime = System.nanoTime();
+
+				switch (func) {
+					case "A":
+						mysteryA(j);
+						break;
+					case "B":  
+						mysteryB(j);
+						break;
+					case "C":
+						mysteryC(j);
+						break;
+					case "D":
+						mysteryD(j);
+						break;
+					case "E":
+						mysteryE(j);
+						break;
+					case "F":
+						mysteryF(j);
+						break;
+				}
+					
+				long estimatedTime = System.nanoTime() - startTime;
+
+				Measurement newMeasurement = new Measurement(func, j, estimatedTime); 
+				list_of_measurements.add(newMeasurement); 
+	
+			}
+
+		}
+		return list_of_measurements; 
 		
-		//Example call to mystery method
-		mysteryA(50);
-		return null;
 	}
 	
 
 	public static String measurementsToCSV(List<Measurement> toConvert) {
-		/** TODO **/
-		return null;
+		
+		String s = ""; 
+
+		System.out.println("name,n,nanoseconds"); 
+
+		for(Measurement m: toConvert) {
+
+			String toAdd = m.name + "," + 
+			String.valueOf(m.valueOfN) + "," + String.valueOf(m.nanosecondsToRun); 
+			System.out.println(toAdd); 
+			s = s + toAdd + '\n'; 
+		}
+
+		return s; 
+
+		
+	
 	}
 	
-	/* Add any helper methods you find useful */
 		
 }
